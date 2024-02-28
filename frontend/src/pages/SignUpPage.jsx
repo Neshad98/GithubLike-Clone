e306b32d76d512fe2e369a2d89964a471a73c1a7
Neshad <djneshad@gmail.com>
2024-02-28 22:47:07 +0600

    first commit

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+
+
+const SignUpPage = () => {
+  return <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+    <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-glass">
+      Signup
+    </div>
+  </div>
+}
+
+export default SignUpPage
