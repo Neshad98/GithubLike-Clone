80a20be04cf553ac3ef5e8423a548f13e7fc0d1d
Neshad <djneshad@gmail.com>
2024-03-08 21:05:18 +0600

    explore repos

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -21,7 +21,7 @@ const HomePage = () => {
       const userRes = await fetch(`https://api.github.com/users/${username}`, {
         headers: {
           // .env is not fckn secured. So use it carefully for production and maybe use it in db
-          authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+          authorization: `token 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`,
         }
       });
       const userProfile = await userRes.json();
