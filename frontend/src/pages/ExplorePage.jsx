734bff1c14782bb7feef7a57380c8122741cf83f
Neshad <djneshad@gmail.com>
2024-03-15 21:53:11 +0600

    working

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -15,13 +15,10 @@ const ExplorePage = () => {
     setRepos([]);
 
     try {
-      const res = await fetch(`https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc&per_page=20`, {
-        // headers: {
-        //   authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
-        // }
-      });
-      const data = await res.json();
-      setRepos(data.items);
+      const res = await fetch("http://localhost:5000/api/explore/repos/" + language);
+      // repos is destructured from the res object
+      const { repos } = await res.json();
+      setRepos(repos);
       setSelectedLanguage(language);
     } catch (error) {
       toast.error(error.message);
