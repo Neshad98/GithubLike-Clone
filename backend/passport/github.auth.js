bed5adf0f080944000147e4a3c2f0093bc7f06d3
Neshad <djneshad@gmail.com>
2024-03-16 22:09:56 +0600

    working

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -22,14 +22,6 @@ passport.use(new GitHubStrategy({
   callbackURL: "/api/auth/github/callback"
 },
   function (accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-
-      // To keep the example simple, the user's GitHub profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the GitHub account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
+    console.log(profile);
   }
 ));
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import passport from 'passport';
 
 const router = express.Router();
 
-router.get("/github");
-router.get("/github/callback");
+router.get("/github", passport.authenticate("github", { scope: ["user:email"] }));
+router.get("/github/callback", passport.authenticate("github", { failureRedirect: process.env.CLIENT_BASE_URL + "/login" }), function (req, res) {
+  res.redirect(process.env.CLIENT_BASE_URL);
+}
+);
 
 
 export default router;
