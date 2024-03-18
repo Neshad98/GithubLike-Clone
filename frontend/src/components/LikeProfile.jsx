0d5f9f0ff3611b1d324ddec2685a86ac84ccb144
Neshad <djneshad@gmail.com>
2024-03-18 17:51:06 +0600

    working

--- frontend/src/components/LikeProfile.jsx
+++ frontend/src/components/LikeProfile.jsx
@@ -1,15 +1,25 @@
 import toast from "react-hot-toast"
 import { FaHeart } from "react-icons/fa"
+import { useAuthContext } from "../context/AuthContext"
 
 
 const LikeProfile = ({ userProfile }) => {
+  const { authUser } = useAuthContext();
   const handleLikeProfile = async () => {
     try {
+      const res = await fetch(`/api/users/like/${userProfile.login}`, {
+        method: "POST",
+        credentials: "include",
+      })
+      const data = await res.json();
+      if (data.error) throw new Error(data.error);
+      toast.success(data.message);
 
     } catch (error) {
       toast.error(error.message);
     }
-  }
+  };
+  if (!authUser) return null;
   return (
     <button className="p-2 text-xs w-full font-medium rounded-md bg-glass border border-blue-400 flex items-center gap-2"
       onClick={handleLikeProfile}
